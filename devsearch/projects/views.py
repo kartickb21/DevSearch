866a08ba8a15fe3d622e079b6f0e612c96e5ba08
Kartick <kartickb21@gmail.com>
2021-10-01 22:59:31 +0530

    CRUD operations added

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 projectsList = [
     {
@@ -21,16 +23,42 @@ projectsList = [
 
 
 def projects(request):
-    page = 'Projects'
-    number = 1
-    context = {"page":page, "number":number, "projects":projectsList}
+    projects = Project.objects.all()
+    context = {"projects":projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i  
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project':projectObj})
 
+def createProject(request):
+    form = ProjectForm()
 
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        else:
+            print('form not valid')
+    context = {"form":form}
+    return render(request, "projects/project_form.html", context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        else:
+            print('form not valid')
+    context = {"form":form}
+    return render(request, "projects/project_form.html", context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
