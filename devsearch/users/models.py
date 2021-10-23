26b17d41eb25a9791361f22c68a25a500aa5e9e3
Kartick <kartickb21@gmail.com>
2021-10-23 20:26:21 +0530

    signals added

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -21,7 +25,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -32,3 +36,5 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+
